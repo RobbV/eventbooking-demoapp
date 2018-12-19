a9df948ea9b1bcfdb450c22d4e2f668df2413b8b
Robbert Van der Grift <rob.vdg.14@gmail.com>
2018-12-18 22:22:14 -0500

    added a user model, and added support for the user in the GraphQL API

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-// import event model
-const Event = require('./models/event')
+// import Models
+const Event = require('./models/event');
+const User = require('./models/user');
 
 const app = express();
+
 const events = [];
 
 
@@ -19,6 +22,7 @@ app.use(bodyParser.json());
 app.use('/graphql', graphqlHttp({
     // schema Object
     schema: buildSchema(`
+
         type Event {
             _id: ID!
             title: String!
@@ -26,7 +30,11 @@ app.use('/graphql', graphqlHttp({
             price: Float!
             date: String!
         }
-
+        type User {
+            _id: ID!
+            email: String!
+            password: String
+        }
         input EventData {
             title: String!
             description: String!
@@ -34,14 +42,18 @@ app.use('/graphql', graphqlHttp({
             date: String!
         }
 
+        input UserData {
+            email : String!
+            password: String!
+        }
         type RootQuery {
             events: [Event!]!
         }
 
         type RootMutation {
             createEvent(eventData: EventData): Event
+            createUser(userData: UserData): User
         }
-
         schema {
             query: RootQuery
             mutation: RootMutation
@@ -49,6 +61,7 @@ app.use('/graphql', graphqlHttp({
     `),
     // resolvers
     rootValue: {
+        // Event Resolvers
        events: () => {
            return Event.find()
                 .then(events => {
@@ -66,21 +79,60 @@ app.use('/graphql', graphqlHttp({
                 title: args.eventData.title,
                 description: args.eventData.description,
                 price: args.eventData.price,
-                date: new Date(args.eventData.date)
+                date: new Date(args.eventData.date),
+                creator: '5c19b29b2340cf188bed0714'
             });
+            // empty variable to store the created event object
+            let createEvent;
+
             return event
             .save()
             .then(result => {
-                console.log(result);
-                return {
-                    // can also us mongoose to turn the mongo ID to string
-                    ...result._doc, _id: event.id
+                createdEvent = {...result._doc, _id: result.id} 
+               return User.findById('5c19b29b2340cf188bed0714') 
+            })
+            .then(user => {
+                if (!user) {
+                    throw new Error('there is no user found')
                 }
-            }).catch(err => {
+                user.createdEvents.push(event);
+                return user.save();
+            }).then(result => {
+                return createdEvent;
+            })
+            .catch(err => {
                 console.log(err);
                 throw err;
             });
+        }, 
+        //=======================End of Event Resolver==================================
+        //User Resolver
+        createUser: args => {
+            // check the DB for email
+            return User.findOne({ email: args.userData.email }).then(user => {
+                // if email is found in the DB then throw an error to prevent duplicate email addresses
+                if (user) {
+                    throw new Error('User already exists!')
+                }
+                // encrypt the users password using bcrypt
+                return bcrypt
+                .hash(args.userData.password, 12)
+            })
+            .then(hashedPassword => {
+                    const user = new User({
+                        email: args.userData.email,
+                        password: hashedPassword
+                    });
+                    return user.save();
+                })
+                .then(result => {
+                    return { ...result._doc, password: null, _id: result.id}
+                })
+                .catch(err => {
+                    throw err;
+                })
         }
+        //=======================End of User  Resolver==================================
     },
     graphiql: true
 }));
